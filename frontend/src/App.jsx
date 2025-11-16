e3cf4614ecd3bc9577d5c4fc8221bbd28eba91e6
Avaneesh <avaneesh40585@gmail.com>
2025-11-16 12:25:51 +0530

    node_modules update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 // Pages
